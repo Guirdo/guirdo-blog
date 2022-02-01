16f2d048924740b3405935461c22049a95d63698
Guirdo <produccionesaldo@gmail.com>
2022-02-01 09:45:50 -0600

    Projects added

--- components/biography.js
+++ components/biography.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from '../styles/biography.module.css'
 
-export default function Biography (){
-    return(
+export default function Biography() {
+    return (
         <section className={styles.container}>
             <div className={styles.profile}>
-                <Image 
+                <Image
                     alt="Seb Méndez"
                     className={styles.profilePhoto}
                     src="/images/profile.jpg"
@@ -16,9 +17,11 @@ export default function Biography (){
 
             <div className={styles.biographyText}>
                 <p>Yo soy Seb Méndez, pero tambien me puedes encontrar como Guirdo. Tengo 24 años y estudie la carrera de Ingenieria en Sistemas Computacionales. Actualmente sigo mis estudios en Platzi y me estoy especializando en el desarrollo front-end, pero tambien he probado tecnologias de back-end. Dale click al botón de abajo.</p>
-                <button className={styles.btnProyectos}>
-                    Echale un vistazo a lo ultimo que he hecho
-                </button>
+                <Link href="/projects">
+                    <button className={styles.btnProyectos}>
+                        Echale un vistazo a lo ultimo que he hecho
+                    </button>
+                </Link>
             </div>
         </section>
     )
---
+++ components/projectCard.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from '../styles/projectCard.module.css'
+
+export default function ProjectCard({ name, description, image, technologies, repository, webSite }) {
+    return (
+        <div className={styles.container}>
+            <Image
+                alt={name}
+                className={styles.image}
+                src={image}
+                width={306}
+                height={200}
+            />
+            <h2 className={styles.title}>{name}</h2>
+            <p className={styles.description}>{description}</p>
+            <small className={styles.technologies}>{technologies}</small>
+            <div className={styles.buttons}>
+                {
+                    repository !== '#' && (
+                        <a href={repository} target="_blank" rel="noopener noreferrer">
+                            <button className={styles.button}>Repositorio</button>
+                        </a>
+                    )
+                }
+                {
+                    webSite !== '#' && (
+                        <a href={webSite} target="_blank" rel="noopener noreferrer">
+                            <button className={styles.button}>Web Site</button>
+                        </a>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
