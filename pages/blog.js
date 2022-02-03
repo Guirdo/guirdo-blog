7c37a31392ff4b9f8085559a2ca27adb37497d95
Guirdo <produccionesaldo@gmail.com>
2022-02-02 20:38:46 -0600

    Blog added

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function BlogPage (){
+    return(
+        <Layout pageTitle="Blog">
+            <h1>My blog</h1>
+        </Layout>
+    )
+} 
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function ContactPage () {
+    return(
+        <Layout pageTitle="Contact me">
+            <h1>Contact me</h1>
+        </Layout>
+    )
+}
