db39f45cb84da230f6d95619efc0350e9ec576aa
Guirdo <produccionesaldo@gmail.com>
2022-02-03 12:44:20 -0600

    UserForm added

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,6 +1,16 @@
 import utils from '../styles/utils/form.module.css'
+import { useForm } from '../hooks/useForm'
 
 export default function ContactForm() {
+
+    const [formValues,handleInputChange] = useForm({
+        name:'',
+        email:'',
+        message: '',
+    })
+
+    const { name,email,message } = formValues
+
     return (
         <form
             className={utils.form}
@@ -11,6 +21,30 @@ export default function ContactForm() {
                 <input
                     placeholder='Name'
                     type="text"
+                    name="name"
+                    value={name}
+                    onChange={handleInputChange}
+                />
+            </div>
+
+            <div className={utils.formGroup}>
+                <label>What's your email address?</label>
+                <input
+                    placeholder='Email'
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>
+
+            <div className={utils.formGroup}>
+                <label>What are you thinking of?</label>
+                <textarea
+                    placeholder="Let's talk"
+                    name="message"
+                    value={message}
+                    onChange={handleInputChange}
                 />
             </div>
         </form>
