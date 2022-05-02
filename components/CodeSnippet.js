fb70da40fa4d985b403ca1db5bcb4a75b1ae6cd0
Guirdo <produccionesaldo@gmail.com>
2022-05-02 14:51:14 -0500

    The last sass migrations' fixes

--- components/CodeSnippet.js
+++ components/CodeSnippet.js
@@ -2,7 +2,7 @@ import styles from '../styles/utils/codeSnippet.module.css'
 
 function CodeSnippet({text}) {
     return (
-        <code className={styles.code}>
+        <code className="code">
             {text}
         </code>
     );
