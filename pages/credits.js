7e0744bfdc1e097f29a1a2dffe037dc764b28103
Guirdo <produccionesaldo@gmail.com>
2022-11-07 14:07:33 -0600

    CHORE: Improve Credits page

--- pages/credits.js
+++ pages/credits.js
@@ -4,32 +4,21 @@ import Layout from "../components/Layout";
 function CreditsPage() {
     return (
         <Layout pageTitle="Credits">
-            <section className="credits-section">
-                <div className="credits">
-                    <Link href="/" className="credits__link">
-                        {'<-'}Back to home
-                    </Link>
+            <div className="credits-section">
+                <div className="credits-body">
+                    <h3 className="credits__title">Créditos</h3>
                     <ul className="credits-list">
                         <li className="credits__list-item">
-                            <b>Background photo: </b>
+                            <b>Imagen de fondo: </b>
                             <a
                                 href="https://www.instagram.com/diana.vzqx/"
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >Dianini</a>
                         </li>
-                        <li className="credits__list-item">
-                            <b>Social networks icons:</b>{' '}
-                            <a
-                                href="https://www.flaticon.es/packs/social-network-3"
-                                title="Social network | Filled"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >Freepik - Flaticon</a>
-                        </li>
                     </ul>
                 </div>
-            </section>
+            </div>
         </Layout>
     );
 }
